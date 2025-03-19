84aaa4c8ae0a1881740c5423b517ea5dd34cfe47
unknown <eocosta@gmail.com>
2025-03-19 17:25:34 -0300

    tentativas de criação das tabelas do banco de dados

--- config/database.py
+++ config/database.py
@@ -4,7 +4,10 @@ db = SQLAlchemy()
 
 def init_db(app):
     """Inicializa o banco de dados no Flask com SQLAlchemy"""
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:sua_senha@localhost/aluguel_cacambas'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/aluguel_cacambas'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:sua_senha@localhost/aluguel_cacambas'
+
 
     db.init_app(app)
